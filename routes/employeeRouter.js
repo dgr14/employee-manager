029766d905503e366fbf1c348c694203fa5babbd
Darius Raazi <dariusraazi@icloud.com>
2019-10-09 15:51:02 -0600

    updating code

--- routes/employeeRouter.js
+++ routes/employeeRouter.js
@@ -4,7 +4,7 @@ const Employee = require('../models/employeeSchema.js')
 
 // GET ALL
 employeeRouter.get("/", (req, res, next) => {
-    // Addition: include fultering criteria to the find so that it only finds todo items with a 'user' property with the current user's id.
+    // Addition: include filtering criteria to the find so that it only finds todo items with a 'user' property with the current user's id.
     Employee.find((err, employees) => {
         if (err) {
             res.status(500)
@@ -44,10 +44,10 @@ employeeRouter.put("/:_id",(req, res, next) => {
         // What will I add to instead of responses
         // Need to figure out how to display the updated data
         newObject,
-        {new: true}, (error, updatedEmployee) => {
-            if(error) {
+        {new: true}, (err, updatedEmployee) => {
+            if(err) {
                 res.status(500)
-                return next(error)
+                return next(err)
             }
             console.log(updatedEmployee)
             return res.status(201).send(updatedEmployee)
@@ -71,23 +71,6 @@ employeeRouter.get('/:employeeId', (req, res, next) => {
         return res.send(employee)
     })
 })
-
-// employeeRouter.put("/:employeeId", (req, res, next) => {
-//     // Addition: Change to findOneAndUpdate and include the query for users
-//     Employee.findOneAndUpdate(
-//         // Updated query to include user
-//         {_id: req.params.employeeId, user: req.user._id},
-//         req.body,
-//         {new: true},
-//         (err, employee) => {
-//             if (err) {
-//                 res.status(500)
-//                 return next(err)
-//             }
-//             return res.send(employee)
-//         }
-//     )
-// })
 
 // Addition: Change to findOneAndRemove and include the search criteria for users
 employeeRouter.delete("/:employeeId", (req, res, next) => {
