7becd33e89c1612935a6c19c6f7171a76a71a9f6
Darius Raazi <dariusraazi@icloud.com>
2019-10-24 18:28:57 -0600

    updating css for design change

--- client/src/IncidentForm.js
+++ client/src/IncidentForm.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import './IncidentForm.css'
+import Employee from './Employee';
 
 function IncidentForm(props) {
     const {handleChange, handleSubmit} = props
+    const {employee} = this.props
     return(
         <div className='incidentFormDiv'>
             <form className='incidentForm'>
+                Employee: ${employee.firstName}
                 {/* Use Employees name from Employee component */}
-                {/* ${inputs.firstName} */}
 
                 <input 
                 name='incidentTitle'
+                onChange={this.handleChange}
+                placeholder={`Incident Title`}
+                className='incidentInput'
                 />
 
                 <input 
                 name='incidentDate'
+                onChange={this.handleChange}
+                placeholder={`Incident Date`}
+                className='incidentInput'
                 />
 
                 <textarea 
                 name='incidentNotes'
+                onChange={this.handleChange}
+                placeholder={`Incident Notes`}
+                className='incidentTextarea'
                 />
             </form>
         </div>
